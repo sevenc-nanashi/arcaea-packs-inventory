3fdbd36305354ec6551751b34bb8e68af914c0fe
sevenc-nanashi <sevenc7c@sevenc7c.com>
2026-01-16 23:16:58 +0900

    feat: 最低限のエンドポイントを実装

--- apps/worker/src/index.ts
+++ apps/worker/src/index.ts
@@ -1,5 +1,31 @@
-export default {
-  async fetch() {
-    return new Response("Hello World!");
+import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
+import { applySerializedInventory } from "@shared/song-data";
+import * as z from "zod";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  // TODO: render html
+  return c.text("Hello, World!");
+});
+
+app.get(
+  "/image",
+  zValidator(
+    "query",
+    z.object({
+      inventory: z.string(),
+    }),
+  ),
+  (c) => {
+    const { inventory: serializedInventory } = c.req.valid("query");
+    const map = new Map();
+    applySerializedInventory(map, serializedInventory);
+		console.log(map);
+
+    return c.text("Image generation not implemented", { status: 501 });
   },
-} satisfies ExportedHandler<Env>;
+);
+
+export default app;
